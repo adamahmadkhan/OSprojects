a0743b6ab73f6f9b78f8c32c2ad011da219ef795
adamahmadkhan <74861984+adamahmadkhan@users.noreply.github.com>
2021-12-25 04:02:43 +0500

    working on about

--- OSproject/Aboutinfo.Designer.cs
+++ OSproject/Aboutinfo.Designer.cs
@@ -29,76 +29,118 @@ namespace OSproject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Aboutinfo));
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.next = new System.Windows.Forms.Button();
+            this.last = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.page1 = new System.Windows.Forms.Label();
+            this.page3 = new System.Windows.Forms.Label();
+            this.page2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // contextMenuStrip1
+            // next
             // 
-            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.next.BackColor = System.Drawing.Color.Lime;
+            this.next.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.next.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next.ForeColor = System.Drawing.Color.White;
+            this.next.Location = new System.Drawing.Point(711, 549);
+            this.next.Margin = new System.Windows.Forms.Padding(4);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(157, 34);
+            this.next.TabIndex = 2;
+            this.next.Text = "Next Page";
+            this.next.UseVisualStyleBackColor = false;
+            this.next.Click += new System.EventHandler(this.next_Click);
             // 
-            // panel1
+            // last
             // 
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(22, 66);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(808, 373);
-            this.panel1.TabIndex = 1;
+            this.last.BackColor = System.Drawing.Color.Lime;
+            this.last.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.last.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.last.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.last.ForeColor = System.Drawing.Color.White;
+            this.last.Location = new System.Drawing.Point(3, 549);
+            this.last.Margin = new System.Windows.Forms.Padding(4);
+            this.last.Name = "last";
+            this.last.Size = new System.Drawing.Size(157, 34);
+            this.last.TabIndex = 3;
+            this.last.Text = "last Page";
+            this.last.UseVisualStyleBackColor = false;
+            this.last.Click += new System.EventHandler(this.last_Click);
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(753, 216);
-            this.label1.TabIndex = 0;
-            this.label1.Text = resources.GetString("label1.Text");
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label1.Visible = false;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(388, 558);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "page0";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
             // 
-            // label3
+            // page1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(118, 216);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(210, 76);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "First come First served (fcfs)\r\nShortest Job first (sjf)\r\npriority scheduling (PS" +
-    ")\r\nRound Robin (RR)";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.page1.AutoSize = true;
+            this.page1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.page1.Location = new System.Drawing.Point(289, 248);
+            this.page1.Name = "page1";
+            this.page1.Size = new System.Drawing.Size(95, 29);
+            this.page1.TabIndex = 0;
+            this.page1.Text = "pagee1";
+            this.page1.Visible = false;
+            this.page1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // page3
+            // 
+            this.page3.AutoSize = true;
+            this.page3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.page3.Location = new System.Drawing.Point(140, 155);
+            this.page3.Name = "page3";
+            this.page3.Size = new System.Drawing.Size(81, 29);
+            this.page3.TabIndex = 0;
+            this.page3.Text = "page3";
+            this.page3.Visible = false;
+            this.page3.Click += new System.EventHandler(this.page3_Click);
+            // 
+            // page2
+            // 
+            this.page2.AutoSize = true;
+            this.page2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.page2.Location = new System.Drawing.Point(140, 248);
+            this.page2.Name = "page2";
+            this.page2.Size = new System.Drawing.Size(81, 29);
+            this.page2.TabIndex = 0;
+            this.page2.Text = "page2";
+            this.page2.Visible = false;
             // 
             // Aboutinfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.page1);
+            this.Controls.Add(this.page3);
+            this.Controls.Add(this.page2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.last);
+            this.Controls.Add(this.next);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Aboutinfo";
             this.Size = new System.Drawing.Size(871, 587);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Load += new System.EventHandler(this.Aboutinfo_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button last;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label page1;
+        private System.Windows.Forms.Label page3;
+        private System.Windows.Forms.Label page2;
     }
 }
--- OSproject/Aboutinfo.cs
+++ OSproject/Aboutinfo.cs
@@ -12,9 +12,24 @@ namespace OSproject
 {
     public partial class Aboutinfo : UserControl
     {
+        uint page = 0;
+        Label[] pg;
         public Aboutinfo()
         {
             InitializeComponent();
+        }
+        private void Aboutinfo_Load(object sender, EventArgs e)
+        {
+            pg= new Label[3];
+            pg[0] = page1;
+            pg[1] = page2;
+            pg[2] = page3;
+            pg[1].Hide();
+            pg[2].Hide();
+            pg[0].Show();
+            
+
+
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -28,6 +43,65 @@ namespace OSproject
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            if (page == 0)
+                last.Show();
+            pg[page].Hide();
+            page = page + 1;
+            page = page % 3;
+            label2.Text = "page" + page;
+            pg[page].Show();
+            
+        }
+
+        private void last_Click(object sender, EventArgs e)
+        {
+            pg[page].Hide();
+            page = page - 1;
+            page = page % 3;
+            if (page == 0)
+                last.Hide();
+            label2.Text = "page" + page;
+            pg[page].Show();
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void page2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void page3_Click(object sender, EventArgs e)
         {
 
         }
