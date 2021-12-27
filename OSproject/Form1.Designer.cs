3e78251f0d9a624ed7f4c58f0b79f1eff023668b
adamahmadkhan <74861984+adamahmadkhan@users.noreply.github.com>
2021-12-27 17:11:00 +0500

    login page changes

--- OSproject/Form1.Designer.cs
+++ OSproject/Form1.Designer.cs
@@ -29,30 +29,32 @@ namespace OSproject
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.name = new System.Windows.Forms.TextBox();
             this.pass = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.sub = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // name
             // 
-            this.name.Location = new System.Drawing.Point(153, 56);
+            this.name.Location = new System.Drawing.Point(204, 69);
+            this.name.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.name.Name = "name";
-            this.name.Size = new System.Drawing.Size(242, 20);
+            this.name.Size = new System.Drawing.Size(321, 22);
             this.name.TabIndex = 0;
             this.name.Text = "Enter Username";
             this.name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // pass
             // 
-            this.pass.Location = new System.Drawing.Point(153, 129);
+            this.pass.Location = new System.Drawing.Point(204, 159);
+            this.pass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pass.Name = "pass";
-            this.pass.Size = new System.Drawing.Size(242, 20);
+            this.pass.Size = new System.Drawing.Size(321, 22);
             this.pass.TabIndex = 1;
             this.pass.Text = "Enter Password";
             this.pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -62,9 +64,10 @@ namespace OSproject
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 45);
+            this.label1.Location = new System.Drawing.Point(4, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 31);
+            this.label1.Size = new System.Drawing.Size(180, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "UserName";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -73,9 +76,10 @@ namespace OSproject
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 118);
+            this.label2.Location = new System.Drawing.Point(4, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.Size = new System.Drawing.Size(167, 39);
             this.label2.TabIndex = 3;
             this.label2.Text = "Password";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -88,9 +92,10 @@ namespace OSproject
             this.sub.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.sub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.sub.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sub.Location = new System.Drawing.Point(153, 218);
+            this.sub.Location = new System.Drawing.Point(204, 268);
+            this.sub.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sub.Name = "sub";
-            this.sub.Size = new System.Drawing.Size(133, 37);
+            this.sub.Size = new System.Drawing.Size(177, 46);
             this.sub.TabIndex = 4;
             this.sub.Text = "Submit";
             this.sub.UseVisualStyleBackColor = false;
@@ -104,39 +109,28 @@ namespace OSproject
             this.panel1.Controls.Add(this.name);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pass);
-            this.panel1.Location = new System.Drawing.Point(388, 12);
+            this.panel1.Location = new System.Drawing.Point(517, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 327);
+            this.panel1.Size = new System.Drawing.Size(533, 402);
             this.panel1.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Modern No. 20", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 117);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(368, 63);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "FA19-BCS-085       Muhammad Tayyab\r\nFA19-BCS-090       Hussnain Aslam\r\nFA19-BCS-1" +
-    "08       Adam Ahmad Khan   ";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel1);
             this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -148,7 +142,6 @@ namespace OSproject
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button sub;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
     }
 }
 
